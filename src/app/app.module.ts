26767bef864358d85171f130230766500e3d3261
waleedkhaled <waleedmini2@gmail.com>
2017-03-07 20:01:23 +0200

    aboutus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { RouterModule }   from '@angular/router';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { SigninComponent } from './signin/signin.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    SigninComponent
+    SigninComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
@@ -21,8 +23,9 @@ import { SigninComponent } from './signin/signin.component';
     HttpModule,
     RouterModule.forRoot([
   {path: 'mainPage',   component: MainPageComponent},
-  {path: '',   component: MainPageComponent}
-  ])
+  {path: '',   component: MainPageComponent},
+  {path: 'aboutus',   component: AboutusComponent}
+  ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
